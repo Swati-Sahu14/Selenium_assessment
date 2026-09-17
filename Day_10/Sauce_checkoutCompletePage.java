ee614798c2f396ef06b343e21915052c42faa778
Swati Sahu <167199129+Swati-Sahu14@users.noreply.github.com>
2026-09-18 00:21:18 +0530

    done
    Updated the message locator and refactored the verifyMessage method to return the message text.

--- Day_10/Sauce_checkoutCompletePage.java
+++ Day_10/Sauce_checkoutCompletePage.java
@@ -6,22 +6,19 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Sauce_checkoutCompletePage {
+
 	WebDriver driver;
 
 	public Sauce_checkoutCompletePage(WebDriver driver) {
-		super();
 		this.driver = driver;
-		PageFactory.initElements(driver,this);
+		PageFactory.initElements(driver , this);
 	}
 	
-	@FindBy(xpath = "//h2")
+	@FindBy(xpath="//h2[text()='Thank you for your order!']")
 	private WebElement message;
 	
-	public void verifyMessage(String value)
-	{
-		if(message.getText().contains(value))
-			System.out.println(value+" is displayed");
-		else
-			System.out.println(value+" is not displayed");
-	}
+	public String getMessage() {
+		return message.getText();
+	}	
+	
 }
