1289beeb6a865bf7aa121b9cee1f6c54439859f6
Swati Sahu <167199129+Swati-Sahu14@users.noreply.github.com>
2026-09-18 00:18:52 +0530

    done

--- Day_10/Sauce_LoginPage.java
+++ Day_10/Sauce_LoginPage.java
@@ -6,32 +6,33 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Sauce_LoginPage {
+
 	WebDriver driver;
 
 	public Sauce_LoginPage(WebDriver driver) {
-		super();
 		this.driver = driver;
-		PageFactory.initElements(driver,this);
+		PageFactory.initElements(driver , this);
 	}
 	
-	@FindBy(id = "user-name")
-	private WebElement untf;
+	@FindBy(id="user-name")
+	private WebElement username;
 	
-	@FindBy(id = "password")
-	private WebElement pwdtf;
+	@FindBy(id="password")
+	private WebElement password;
 	
-	@FindBy(id = "login-button")
-	private WebElement loginButton;
-	
-	public void getUntf(String value) {
-		untf.sendKeys(value);
+	@FindBy(id="login-button")
+	private WebElement login;
+
+	public void getUsername(String value) {
+		username.sendKeys(value);
 	}
 
-	public void getPwdtf(String value) {
-		pwdtf.sendKeys(value);
+	public void getPassword(String value) {
+		password.sendKeys(value);
 	}
 
-	public void getLoginButton() {
-		loginButton.click();
+	public void getLogin() {
+		login.click();
 	}
+		
 }
