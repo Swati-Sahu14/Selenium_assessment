417a60bd69331811c9c8c78bbbb0c6238f284f59
Swati Sahu <167199129+Swati-Sahu14@users.noreply.github.com>
2026-09-18 00:19:40 +0530

    done
    Refactor Sauce_HomePage class by updating WebElement annotations and method names for clarity.

--- Day_10/Sauce_HomePage.java
+++ Day_10/Sauce_HomePage.java
@@ -6,44 +6,41 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Sauce_HomePage {
-	WebDriver driver;
 
+	WebDriver driver;
+	
 	public Sauce_HomePage(WebDriver driver) {
-		super();
 		this.driver = driver;
-		PageFactory.initElements(driver,this);
+		PageFactory.initElements(driver , this);
 	}
 	
-	@FindBy(xpath = "//div[text()='Sauce Labs Backpack']/../../..//button")
-	private WebElement addToCart;
+	@FindBy(xpath="//span[text()='Products']")
+	private WebElement product;
 	
-	@FindBy(xpath = "//span[text()='1']")
+	@FindBy(id="add-to-cart-sauce-labs-backpack")
+	private WebElement addtocart;
+	
+	@FindBy(className="shopping_cart_badge")
+	private WebElement quantity;
+	
+	@FindBy(xpath="//a[@class='shopping_cart_link']")
 	private WebElement cart;
 	
-	public void clickAddToCart()
-	{
-		addToCart.click();
+	
+
+	public String getProduct() {
+		return product.getText();
+	}
+
+	public void getAddtocart() {
+		addtocart.click();
 	}
 	
-	public boolean verifyProductPage(String value)
-	{
-		
-		if(driver.getCurrentUrl().contains(value))
-			return true;
-		else
-			return false;
+	public String getquantity() {
+		return quantity.getText();
 	}
-	
-	public void verifyCartContains1item(String value)
-	{
-		if(cart.getText().equals(value))
-			System.out.println("Cart contains 1 item");
-		else
-			System.out.println("Cart does not contain 1 item");
-	}
-	
-	public void clickCartLink()
-	{
+
+	public void getCart() {
 		cart.click();
 	}
 }
